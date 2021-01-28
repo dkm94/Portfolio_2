e9d9a400089a1f9fda61c138b062e7fb6368bb30
dkm94 <diane.mpacko@gmail.com>
2021-01-28 09:56:09 +0100

    update stack

--- src/Components/Présentation/Présentation.js
+++ src/Components/Présentation/Présentation.js
@@ -34,7 +34,7 @@ const Présentation = () => {
                 <h2 style={{ textAlign: "end"}}>Qui suis-je ?</h2>   
                 <p>Après des études de commerce international et de traduction, j'ai développé une appétence pour les métiers du numérique qui me permettent à présent de faire partie de la grande famille des développeurs web.
                 Ma maîtrise de l'anglais a été un atout durant ma formation de Développeur web fullstack JS, et reste une ressource indispensable dans l'apprentissage continu de nouveaux outils/langages et veilles technologiques.<br />
-                Je suis ouverte aux projets MERN Stack en frontend, backend et fullstack. En tant que junior, je suis prête à mettre en application toutes mes connaissances acquises depuis le début de mon parcours dans le développement et à donner mon maximum afin d'apporter une vraie plus value à votre équipe ! 
+                Je suis ouverte aux projets frontend, backend et fullstack. En tant que junior, je suis prête à mettre en application toutes mes connaissances acquises depuis le début de mon parcours dans le développement et à donner mon maximum afin d'apporter une vraie plus value à votre équipe ! 
                 </p>
                 <div className="intro center-x intro__recto recto" style={visible ? show : hide}>
                     <span style={{ fontSize: "1.3rem" }}>QUI SUIS-JE ?</span>
